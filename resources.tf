0e41e53f955a3bb0a5ed362ed516b66c215e2db3
Yevgeni Shapiro <23049337+app1980max@users.noreply.github.com>
2026-03-14 19:51:53 +0200

    update

--- resources.tf
+++ resources.tf
@@ -28,16 +28,16 @@ module "vcluster-platform" {
   depends_on = [module.nginx]
 }
 
-module "vclusters" {
-  source = "./modules/vclusters"
-  depends_on = [module.vcluster-platform]
-  for_each = {
-    dev  = "dev"
-    qa   = "qa"
-  }
-
-  name      = "vcluster-${each.key}"
-  namespace = "vcluster-${each.key}"
-}
+#module "vclusters" {
+#  source = "./modules/vclusters"
+#  depends_on = [module.vcluster-platform]
+#  for_each = {
+#    dev  = "dev"
+#    qa   = "qa"
+#  }
+#
+#  name      = "vcluster-${each.key}"
+#  namespace = "vcluster-${each.key}"
+#}
 
 
