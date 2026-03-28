3f1fa8b27f721c8f0a1fbb0818af27dff2095fd4
app1980max <23049337+app1980max@users.noreply.github.com>
2026-03-28 22:46:03 +0300

    update
    Removed unused variables related to loft configuration.

--- modules/vclusters/variable.tf
+++ modules/vclusters/variable.tf
@@ -1,14 +1,5 @@
-
 variable "name" {}
 variable "namespace" {}
-
-variable "loft_host" {}
-variable "loft_user" {}
-variable "loft_password" {}
-
-variable "loft_project" {
-  default = "Default Project"
-}
 
 variable "kubeconfig_path" {
   description = "Number of additional control planes for HA"
