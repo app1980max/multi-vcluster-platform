72e420e3ddbab3b1027e934b88258e2719abd4d7
app1980max <23049337+app1980max@users.noreply.github.com>
2026-03-28 22:45:38 +0300

    update
    Removed the null_resource for cluster registration in Loft.

--- modules/vclusters/main.tf
+++ modules/vclusters/main.tf
@@ -9,33 +9,3 @@ resource "helm_release" "vcluster" {
   wait    = true
   timeout = 600
 }
-
-resource "null_resource" "register" {
-
-  provisioner "local-exec" {
-
-    environment = {
-      LOFT_HOST     = var.loft_host
-      LOFT_USERNAME = var.loft_user
-      LOFT_PASSWORD = var.loft_password
-      KUBECONFIG    = var.kubeconfig_path
-    }
-
-    command = <<EOT
-set -e
-echo "Checking cluster ${var.name} in Loft..."
-if loft cluster get ${var.name} --project "${var.loft_project}" &> /dev/null; then
-  echo "Cluster ${var.name} already registered in Loft"
-else
-  echo "Registering cluster ${var.name} in Loft project ${var.loft_project}"
-
-  loft cluster add \
-    --name ${var.name} \
-    --namespace ${var.namespace} \
-    --project "${var.loft_project}"
-fi
-EOT
-  }
-
-  depends_on = [helm_release.vcluster]
-}
