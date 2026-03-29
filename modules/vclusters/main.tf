a825d1e35a5e7bdc03a9be32c89047113538ee2a
app1980max <23049337+app1980max@users.noreply.github.com>
2026-03-30 01:21:37 +0300

    update

--- modules/vclusters/main.tf
+++ modules/vclusters/main.tf
@@ -8,4 +8,32 @@ resource "helm_release" "vcluster" {
 
   wait    = true
   timeout = 600
+
+  # --- control plane ---
+  set {
+    name  = "controlPlane.coredns.embedded"
+    value = "true"
+  }
+
+  # --- sync from host ---
+  set {
+    name  = "sync.fromHost.ingressClasses.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "sync.fromHost.nodes.enabled"
+    value = "true"
+  }
+
+  # --- sync to host ---
+  set {
+    name  = "sync.toHost.ingresses.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "sync.toHost.services.enabled"
+    value = "true"
+  }
 }
