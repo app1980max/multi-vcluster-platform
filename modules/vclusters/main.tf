952c8cfea3a167c68e176db0fc9a1f9397c90102
app1980max <23049337+app1980max@users.noreply.github.com>
2026-04-01 03:16:46 +0300

    update
    Removed unnecessary control plane and sync settings.

--- modules/vclusters/main.tf
+++ modules/vclusters/main.tf
@@ -12,24 +12,7 @@ resource "helm_release" "vcluster" {
   # Use external values.yaml for all configuration
   values = [file("${path.module}/vcluster-values.yaml")]
 
-  # --- control plane ---
-  set {
-    name  = "controlPlane.coredns.embedded"
-    value = "true"
-  }
 
-  # --- sync from host ---
-  set {
-    name  = "sync.fromHost.ingressClasses.enabled"
-    value = "true"
-  }
-
-  set {
-    name  = "sync.fromHost.nodes.enabled"
-    value = "true"
-  }
-
-  # --- sync to host ---
   set {
     name  = "sync.toHost.ingresses.enabled"
     value = "true"
@@ -39,4 +22,7 @@ resource "helm_release" "vcluster" {
     name  = "sync.toHost.services.enabled"
     value = "true"
   }
+
 }
+
+
